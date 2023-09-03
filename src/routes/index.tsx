858717a57eec60e8718fb438b42c59c535c00cd2
Santiago Ariel Villanueva <santiagoarielv98@gmail.com>
2023-09-02 21:27:02 -0300

    update react router dom basename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,20 +4,25 @@ import { restCountriesPath } from '../pages/advanced/rest-countries/route'
 
 const defaultPath = `${advancedPath}/${restCountriesPath}`
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      children: [advancedRoutes]
+    },
+    {
+      path: '*',
+      children: [
+        {
+          index: true,
+          element: <Navigate to={defaultPath} replace={true} />
+        }
+      ]
+    }
+  ],
   {
-    path: '/',
-    children: [advancedRoutes]
-  },
-  {
-    path: '*',
-    children: [
-      {
-        index: true,
-        element: <Navigate to={defaultPath} replace={true} />
-      }
-    ]
+    basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'
   }
-])
+)
 
 export default router
