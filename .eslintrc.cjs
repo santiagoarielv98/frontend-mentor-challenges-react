cfadc66a421f79ea416ac0a0bcbb887f5dd06c30
Santiago Ariel Villanueva <santiagoarielv98@gmail.com>
2023-09-03 12:41:46 -0300

    config eslint & update readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: ['./tsconfig.json', './tsconfig.node.json']
   },
   plugins: ['react'],
   rules: {
